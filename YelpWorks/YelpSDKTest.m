0e5baf045b34ddea0697e4978015f43c878bad81
Zarph <ramiro@leapcommerce.com>
2013-03-26 23:05:05 -0300

    commit

--- YelpWorks/YelpSDKTest.m
+++ YelpWorks/YelpSDKTest.m
@@ -8,21 +8,21 @@
 
 #import "YelpSDKTest.h"
 
-static YelpSDKTest *yelp;
-
 @implementation YelpSDKTest
 
-+ (YelpSDKTest *)sharedClient{
-    if (!yelp)
-        yelp = [[YelpSDKTest alloc] init];
++ (YelpSDKTest *)sharedClient {
+    static YelpSDKTest *_sharedClient = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        _sharedClient = [[YelpSDKTest alloc] initWithBaseURL:[NSURL URLWithString:@"http://api.yelp.com/v2/"] key:@"CCWA9Km2t8lC7BFRVi2IkA" secret:@"IRdQKHOEvJJGcuHz_j0zYsZaLg8"];
+        
+    });
     
-    return yelp;
+    return _sharedClient;
 }
 
 -(void)authorizeWorks{
-    
-    yelp = [self initWithBaseURL:[NSURL URLWithString:@"http://api.yelp.com/v2/"] key:@"CCWA9Km2t8lC7BFRVi2IkA" secret:@"IRdQKHOEvJJGcuHz_j0zYsZaLg8"];
-    
     AFOAuth1Token *token = [[AFOAuth1Token alloc] initWithKey:@"e7nKgPCRAIAAEts_q0YcbK_gKYDnJF5C" secret:@"CN8R7f1YA3l30GdQl1cQB7a-mvw" session:nil expiration:nil renewable:NO];
     
     [self acquireOAuthAccessTokenWithPath:@"http://api.yelp.com/v2/search?term=restaurants&location=new%20york" requestToken:token accessMethod:@"GET" success:^(AFOAuth1Token *accessToken) {
