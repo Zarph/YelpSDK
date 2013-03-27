3ee3dc2de206711b9c30aa3754733075eeded178
Zarph <ramiro@leapcommerce.com>
2013-03-26 23:10:14 -0300

    commit

--- YelpWorks/YelpSDKTest.m
+++ YelpWorks/YelpSDKTest.m
@@ -5,8 +5,15 @@
 //  Created by Ramiro Guerrero on 26/03/13.
 //  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
 //
-
 #import "YelpSDKTest.h"
+
+
+static NSString * const kOAuth1BaseURLString = @"http://api.yelp.com/v2/";
+static NSString * const kConsumerKeyString = @"";
+static NSString * const kConsumerSecretString = @"";
+static NSString * const kTokenString = @"";
+static NSString * const kTokenSecretString = @"";
+
 
 @implementation YelpSDKTest
 
@@ -15,7 +22,7 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         
-        _sharedClient = [[YelpSDKTest alloc] initWithBaseURL:[NSURL URLWithString:@"http://api.yelp.com/v2/"] key:@"CCWA9Km2t8lC7BFRVi2IkA" secret:@"IRdQKHOEvJJGcuHz_j0zYsZaLg8"];
+        _sharedClient = [[YelpSDKTest alloc] initWithBaseURL:[NSURL URLWithString:kOAuth1BaseURLString] key:kConsumerKeyString secret:kConsumerSecretString];
         
     });
     
@@ -23,7 +30,7 @@
 }
 
 -(void)authorizeWorks{
-    AFOAuth1Token *token = [[AFOAuth1Token alloc] initWithKey:@"e7nKgPCRAIAAEts_q0YcbK_gKYDnJF5C" secret:@"CN8R7f1YA3l30GdQl1cQB7a-mvw" session:nil expiration:nil renewable:NO];
+    AFOAuth1Token *token = [[AFOAuth1Token alloc] initWithKey:kTokenString secret:kTokenSecretString session:nil expiration:nil renewable:NO];
     
     [self acquireOAuthAccessTokenWithPath:@"http://api.yelp.com/v2/search?term=restaurants&location=new%20york" requestToken:token accessMethod:@"GET" success:^(AFOAuth1Token *accessToken) {
         
